a02f1d149d772fe73dd725a5c491fb88bf89f395
amalakamala <amalakamala.a@gmail.com>
2017-07-07 17:35:31 -0400

    modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,78 @@
 $(document).ready(function() {
 
-	/*
-	$(window).load(function() {
-	    
-	});	
-*/
 
 	var mostrarPoke = function(data){
-		var nombre = "";
-		data.forEach(function(element){
-			nombre = element.pokemon_species.name;
-			//$("#los-poke").append(imprimirImg(exports.getSprite(name)));
-			$("#los-poke").append(imprimir(nombre));			
-		});
-	}
+		data.pokemon_entries.forEach(function(element){
+			var nombre = element.pokemon_species.name;
+			var id = element.entry_number;
+			var linkImg = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+			$("#los-poke").append(`
+  			<div class='col l2 center caja-poke'>
+				<a href='#modal-`+id +`' id='`+ id +`' class='btn-poke-modal' ><img class='fondo-img caja' src='`+ linkImg + id + `.png'></img></a>
+				<div class='pie-fondo'>
+					<div class='pie-foto'>
+						<div class='iconos'>
+							<div class='colection-iconos'>
+								<img src='assets/img/icon/pokeball_gray.png'></img>
+								<img src='assets/img/icon/valentines-heart.png'></img>
+								<img src='assets/img/icon/data.png'></img>
+							</div>
+						</div>
+					</div>
+					<p class='name'>` + nombre + `</p>
+				</div>
+			</div>`).append(pokemonDetalle(id,nombre));
+
+			
+			$('.modal').modal();
 
 
-	var imprimir = function(name){
-		var t = "<div clas='col l3 name'>" + name + "</div>";
-		return t ;
+		})
+
+		/*
+			$(".btn-poke-modal").click(function(){
+				console.log("hola");	
+				var apiPo = "http://pokeapi.co/api/v2/pokemon-species/1/";
+
+				$.ajax({
+					url: apiPo + "pokemon/" 
+				})
+				.done(function(response){
+					$("#nombre-pokemon-m").html(response.name);
+				})
+			});
+		*/
+	};
+
+
+
+	function pokemonDetalle(id,nombre){
+		var elModal = `<div id='modal-`+id +`' class="modal">
+		<div id="info-modal" class="modal-content">
+			<div class="row">
+				<div class="col l6">
+					
+				</div>
+				<div class="col l6">
+					<h1 id="nombre-pokemon-m">` + nombre + `</h1>
+				</div>				
+			</div>
+		</div>
+	</div>`;
+	return elModal
 	}
 
-	/*
-	var imprimirImg = function(name){
-		var t = "<div class='elemento'><img src='" + name + "'/></div>";
-		return t ;
-	}
-	*/
 
 	var ajaxPoke = function(name){
+
 		$.ajax({
 			url: 'http://pokeapi.co/api/v2/pokedex/1/',
 			type: 'GET',
 			datatype: 'json',
 		})
 		.done(function(response){
-			console.log(response.pokemon_entries);
-			mostrarPoke(response.pokemon_entries);
+			mostrarPoke(response);
 		})
 		.fail(function(){
 			console.log("error");
@@ -48,21 +84,7 @@ $(document).ready(function() {
 		ajaxPoke(name);
 	});
 
-
-
-/*
-	var poke = "http://pokeapi.co/api/v2/pokemon-species/1";
-	poke.forEach(function(el){console.log(el);})
-
-
-	$("#btn-buscar").click(function(e){
-		$("#los-poke").empty();
-
-		var buscar = $("#buscar").val();
-		var apiPokemon = "http://pokeapi.co/api/v2/pokemon-species/1" + buscar;
-
-
-	})
-
-*/	
 });
+
+
+
